16a3e4a78f79494d435e88d16c07dacf6471a8df
github username <github email address>
2018-07-27 20:49:03 -0400

    add redux/create delete and add item

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Provider } from 'react-redux';
+import { Container } from 'reactstrap'
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar/>
+          <Container>
+            <ItemModal/>
+            <ShoppingList/>
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
